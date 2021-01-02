f38402ba64f5c532ba3c41de8576cb84297d94b4
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2021-01-02 18:29:49 +0100

    literate and examples in docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,23 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, gmsh
 
+include("generate.jl")
+
+GENERATEDEXAMPLES = [joinpath("examples", f) for f in (
+    "t1.md",
+    "t2.md",
+    "t3.md",
+    "t4.md",
+    "t5.md",
+    "t10.md",
+    "t16.md"
+    )]
+
 makedocs(sitename="Gmsh API for Julia",
          modules=[gmsh],
          authors="Maximilian Köhler",
          pages=["Home"=> "index.md",
+                "Tutorial" => GENERATEDEXAMPLES,
                 "Built-in Geometry Kernel" => Any["Geometry" => "geo/geometry.md", 
                                                   "Mesh Constraints" => "geo/mesh.md"],
                 "Model" => Any["Model" => "model/model.md", 
